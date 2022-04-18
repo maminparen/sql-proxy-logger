1bf26866f52e4e39fd58fab5d9e8084597d03a10
Herschel Zona <hzona@ev-n4.kzn.21-school.ru>
2022-04-18 19:32:24 +0300

    visual

--- Server.cpp
+++ Server.cpp
@@ -67,40 +67,40 @@ void    Server::start() {
 
 void Server::poll_in(pollfdType::iterator &it)
 {
-	it->revents = 0;
-	sockaddr_in	addr;
-	socklen_t addr_len = sizeof(addr);
-	int user_fd = accept(it->fd, reinterpret_cast<struct sockaddr*>(&addr), &addr_len);
-	if (user_fd < 0)
-		std::cout << "Сокет не откры для пользователя" << std::endl;
-	else {
-		char buffer[16];
-		inet_ntop( AF_INET, &addr.sin_addr, buffer, sizeof(buffer));
-		
-		User *new_user = new User(user_fd, &_address_bd);
+    it->revents = 0;
+    sockaddr_in	addr;
+    socklen_t addr_len = sizeof(addr);
+    int user_fd = accept(it->fd, reinterpret_cast<struct sockaddr*>(&addr), &addr_len);
+    if (user_fd < 0)
+        std::cout << "Сокет не откры для пользователя" << std::endl;
+    else {
+        char buffer[16];
+        inet_ntop( AF_INET, &addr.sin_addr, buffer, sizeof(buffer));
 
-		struct pollfd tmp;
-		tmp.fd = user_fd;
-		tmp.events = POLLIN;
-		tmp.revents = 0;
+        User *new_user = new User(user_fd, &_address_bd);
 
-		if (fcntl(user_fd, F_SETFL, O_NONBLOCK) < 0) {
+        struct pollfd tmp;
+        tmp.fd = user_fd;
+        tmp.events = POLLIN;
+        tmp.revents = 0;
+
+        if (fcntl(user_fd, F_SETFL, O_NONBLOCK) < 0) {
             std::cout << "Ошибка fcntl() пользователь: " << user_fd << " адрес: " << buffer << std::endl;
-			close(user_fd);
-			return;
-		}
+            close(user_fd);
+            return;
+        }
 
         int enable = 1;
         if (setsockopt(user_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1) {
             std::cout << "Ошибка setsockopt() пользователь: " << user_fd << " адрес: " << buffer << std::endl;
-			close(user_fd);
-			return;
+            close(user_fd);
+            return;
 		}
 
-		_fds.push_back(tmp);
+        _fds.push_back(tmp);
         tmp.fd = new_user->get_dbSc();
         _fds.push_back(tmp);
-		_users.insert(std::pair<int, User *>(user_fd, new_user));
+        _users.insert(std::pair<int, User *>(user_fd, new_user));
         _users.insert(std::pair<int, User *>(new_user->get_dbSc(), new_user));
         std::cout << "Соединение установлено, пользователь: " << user_fd << " адрес: " << buffer << std::endl;
 	}
@@ -108,16 +108,16 @@ void Server::poll_in(pollfdType::iterator &it)
 
 int Server::poll_user(pollfdType::iterator &it)
 {
-	it->revents = 0;
-	User *itu = _users[it->fd];
+    it->revents = 0;
+    User *itu = _users[it->fd];
 
-	char buffer[MAX_BUFFER_RECV + 1];
-	int nbytes = recv(it->fd, buffer, MAX_BUFFER_RECV, 0);
-	if (nbytes <= 0) 
-		return 0;
-	else {
+    char buffer[MAX_BUFFER_RECV + 1];
+    int nbytes = recv(it->fd, buffer, MAX_BUFFER_RECV, 0);
+    if (nbytes <= 0) 
+        return 0;
+    else {
         buffer[nbytes] = '\0';
-		itu->check_req(buffer, nbytes);
+        itu->check_req(buffer, nbytes);
         if (nbytes != MAX_BUFFER_RECV) {
             if (it->fd == itu->get_dbSc())
                 send(itu->get_userSc(), itu->getMsg().c_str(),itu->getMsg().length() ,0);
@@ -129,29 +129,24 @@ int Server::poll_user(pollfdType::iterator &it)
     return 1;
 }
 
-int Server::poll_out(pollfdType::iterator &it) {
-    
-    return 1;
-}
-
 void Server::loop() {
     while (true)
-	{
-		if (poll(&(_fds.front()), _fds.size(), 0) < 0) {
-			std::cout << (std::string)__func__ + ": poll()" << std::endl;
+    {
+        if (poll(&(_fds.front()), _fds.size(), 0) < 0) {
+            std::cout << (std::string)__func__ + ": poll()" << std::endl;
             continue;
         }
 
-		for (pollfdType::iterator it = _fds.begin(), itend = _fds.end(); it != itend; ++it)
-		{
-			if (it->revents == 0)
-				continue;
+        for (pollfdType::iterator it = _fds.begin(), itend = _fds.end(); it != itend; ++it)
+        {
+            if (it->revents == 0)
+                continue;
 
-			if (it->revents & POLLIN && it->fd == _sc) {
-				poll_in(it);
-				break;
-			}
-			else if (!(it->revents & POLLIN && poll_user(it)) ){
+            if (it->revents & POLLIN && it->fd == _sc) {
+                poll_in(it);
+                break;
+            }
+            else if (!(it->revents & POLLIN && poll_user(it)) ){
                 close(it->fd);
                 int tmp = _users[it->fd]->get_dbSc();
                 if (it->fd == tmp)
@@ -168,6 +163,6 @@ void Server::loop() {
                     }
                 }
             }
-		}
-	}
+        }
+    }
 }
